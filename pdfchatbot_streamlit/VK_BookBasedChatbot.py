f3c9564d320479d0945fe0de849373da83628b15
Vk <venkatkumar2020ece@gmail.com>
2023-06-04 18:11:52 +0530

    Finally project is completed

--- pdfchatbot_streamlit/VK_BookBasedChatbot.py
+++ pdfchatbot_streamlit/VK_BookBasedChatbot.py
@@ -4,33 +4,60 @@ from PyPDF2 import PdfFileReader, PdfFileWriter,PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
+from langchain.llms import OpenAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain.callbacks import get_openai_callback
+
 import pickle
 import os
 #load api key lib
 from dotenv import load_dotenv
+import base64
+
+
+#Background images add function
+def add_bg_from_local(image_file):
+    with open(image_file, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+    st.markdown(
+    f"""
+    <style>
+    .stApp {{
+        background-image: url(data:image/{"jpeg"};base64,{encoded_string.decode()});
+        background-size: cover;
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+    )
+#add_bg_from_local('images.jpeg')  
 
 #sidebar contents
 
 with st.sidebar:
-    st.title('VK pdf LLM chat APP')
+    st.title('🦜️🔗VK - PDF BASED LLM-LANGCHAIN CHATBOT🤗')
     st.markdown('''
-    ## About:
+    ## About APP:
 
-    Main resource of the app is used to build:
+    The app's primary resource is utilised to create::
 
     - [streamlit](https://streamlit.io/)
     - [Langchain](https://docs.langchain.com/docs/)
     - [OpenAI](https://openai.com/)
+
+    ## About me:
+
+    - [Linkedin](https://www.linkedin.com/in/venkat-vk/)
     
     ''')
 
     add_vertical_space(4)
-    st.write('All about pdf based chatbot, created by VK')
+    st.write('💡All about pdf based chatbot, created by VK🤗')
 
 load_dotenv()
 
 def main():
-    st.header("Chat with your pdf file")
+    st.header("📄Chat with your pdf file🤗")
 
     #upload a your pdf file
     pdf = st.file_uploader("Upload your PDF", type='pdf')
@@ -76,10 +103,21 @@ def main():
         
         #Accept user questions/query
 
-        query = st.text_input("Ask questions about your pdf file")
-        st.write(query)
+        query = st.text_input("Ask questions about related your upload pdf file")
+        #st.write(query)
 
-
+        if query:
+            docs = vectorstore.similarity_search(query=query,k=3)
+            #st.write(docs)
+            
+            #openai rank lnv process
+            llm = OpenAI(temperature=0)
+            chain = load_qa_chain(llm=llm, chain_type= "stuff")
+            
+            with get_openai_callback() as cb:
+                response = chain.run(input_documents = docs, question = query)
+                print(cb)
+            st.write(response)
 
 
 
